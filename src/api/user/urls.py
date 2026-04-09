0f587b4ce4061f77ded81afd3fd71ea8f54bdd31
Hiennguyen278610 <thanhhien00000005@gmail.com>
2026-04-10 04:42:24 +0700

    feat(user): implement block user endpoint for admin

--- src/api/user/urls.py
+++ src/api/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from src.api.user.views import UserListView, UserDetailView
+from src.api.user.views import UserListView, UserDetailView, BlockUserView
 
 urlpatterns = [
     path("", UserListView.as_view()),
     path("<int:user_id>", UserDetailView.as_view()),
+    path("<int:user_id>/block", BlockUserView.as_view()),
 ]
