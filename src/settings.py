1c2272c67e04794f3664c7b51ec63c6b7b6ed46e
Hiennguyen278610 <thanhhien00000005@gmail.com>
2026-04-10 03:47:41 +0700

    chore(system): cors local config

--- src/settings.py
+++ src/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "src.core",
     "src.models",
 ]
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -149,3 +151,14 @@ REFRESH_TOKEN_COOKIES_NAME = "refresh_token"
 REFRESH_TOKEN_COOKIES_HTTPONLY = True
 REFRESH_TOKEN_COOKIES_SAMESITE = "Lax"
 REFRESH_TOKEN_COOKIES_SECURE = not DEBUG
+
+# CORS settings
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Vite dev server
+    "http://127.0.0.1:5173",
+]
+
+# Allow credentials (cookies, authorization headers)
+CORS_ALLOW_CREDENTIALS = True
+
+# Internationalization
