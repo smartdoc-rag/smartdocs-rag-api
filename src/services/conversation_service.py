acf601d5306ef7caef352ae9f6dda0f9b8ecbcda
Hiennguyen278610 <thanhhien00000005@gmail.com>
2026-04-20 00:29:57 +0700

    refactor(service): dependencies injection for service

--- src/services/conversation_service.py
+++ src/services/conversation_service.py
@@ -1,72 +1,85 @@
 from src.repositories.conversation_repository import ConversationRepository
 from src.models.conversations import Conversation
 from django.utils import timezone
+from src.core.exceptions import NotFoundException, ForbiddenException
+
 
 class ConversationService:
-    def __init__(self):
-        self.conversation_repo = ConversationRepository()
+    def __init__(self, conversation_repo: ConversationRepository):
+        self.conversation_repo = conversation_repo
 
-    #tao session cua user
+    # tao session cua user
     def create_conversation(self, user_id: int, title: str = None) -> Conversation:
         if not user_id:
-            raise ValueError("user_id is required")
-        conv = Conversation(user_id=user_id, title=title, last_chat_at= timezone.now())
+            raise ValueError("không tìm thấy người dùng")
+        conv = Conversation(user_id=user_id, title=title, last_chat_at=timezone.now())
         return self.conversation_repo.create(conv)
 
     # lay session cua user
     def get_user_conversations(self, user_id: int, skip=0, limit=20):
         return self.conversation_repo.get_all(user_id=user_id, skip=skip, limit=limit)
-    
-    
-    #update title
-    def update_conversation(self, user_id: int, conversation_id: int, title: str = None) -> Conversation:
+
+    # update title
+    def update_conversation(
+        self, user_id: int, conversation_id: int, title: str = None
+    ) -> Conversation:
         if not user_id or not conversation_id:
             raise ValueError("user_id and conversation_id are required")
-        
-        conv = self.conversation_repo.get_user_conversation_by_id(user_id, conversation_id)
+
+        conv = self.conversation_repo.get_user_conversation_by_id(
+            user_id, conversation_id
+        )
         if not conv:
-            raise ValueError("Conversation not found")
-        
+            raise ("Conversation not found")
+
         conv.title = title
         return self.conversation_repo.update(conv)
-    
 
-    #update last_chat
-    def update_last_chat(self, user_id: int, conversation_id: int, title: str = None) -> Conversation:
+    # update last_chat
+    def update_last_chat(
+        self, user_id: int, conversation_id: int, title: str = None
+    ) -> Conversation:
         if not user_id or not conversation_id:
             raise ValueError("user_id and conversation_id are required")
-        
-        conv = self.conversation_repo.get_user_conversation_by_id(user_id, conversation_id)
+
+        conv = self.conversation_repo.get_user_conversation_by_id(
+            user_id, conversation_id
+        )
         if not conv:
-            raise ValueError("Conversation not found")
-        
+            raise NotFoundException("Không tìm tìm thấy đoạn chat")
+
         conv.last_chat_at = timezone.now()
         return self.conversation_repo.update(conv)
-    
-    
+
     def delete_conversation(self, user_id: str, conversation_id: str) -> bool:
         if not user_id:
             raise ValueError("user_id is required")
-        
+
         if not conversation_id:
             raise ValueError("conversation_id is required")
-        
-        conv = self.conversation_repo.get_user_conversation_by_id(user_id, conversation_id)
-        
+
+        conv = self.conversation_repo.get_user_conversation_by_id(
+            user_id, conversation_id
+        )
+
         if not conv:
-            raise ValueError("Conversation not found")
-        
+            raise NotFoundException("Không tìm tìm thấy đoạn chat")
+
         deleted_count = conv.delete()
-        
+
         if not deleted_count:
             raise RuntimeError("Failed to delete conversation")
-        
-        return True
-        
 
-    #lay session theo id cua user
-    def get_conversation_by_id(self, conversation_id: int, user_id: int):
-        conv = self.conversation_repo.get_user_conversation_by_id(user_id, conversation_id)
-        if not conv:
-            raise PermissionError("Conversation not found or access denied")
+        return True
+
+    # lay session theo id cua user
+    def get_conversation_by_id(
+        self, conversation_id: int, user_id: int
+    ) -> Conversation:
+        conv = self.conversation_repo.get_user_conversation_by_id(
+            user_id, conversation_id
+        )
+        raise ForbiddenException(
+            "Cuộc hội thoại không tồn tại hoặc không có quyền truy cập"
+        )
         return conv
